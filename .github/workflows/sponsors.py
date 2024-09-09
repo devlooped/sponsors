e9e2a513ad2093b9366a0d9562465346f35a7a20
Daniel Cazzulino <daniel@cazzulino.com>
2024-09-09 19:21:01 -0300

    Read SVG as source, use CSS and flags to fix cropping

--- .github/workflows/sponsors.py
+++ .github/workflows/sponsors.py
@@ -3,11 +3,24 @@ import sys
 from html2image import Html2Image
 
 chrome = sys.argv[1]
-input = sys.argv[2]
+# read entire contents of file in args[2] as input
+with open(sys.argv[2], 'r') as file:
+    input = file.read()
+    
 output = sys.argv[3]
 
 hti = Html2Image(size=(38,38), browser_executable=chrome)
 hti.browser.print_command = True
-hti.browser.flags = ['--default-background-color=00000000', '--headless', '--disable-remote-debugging', '--no-sandbox'];
+hti.browser.flags = ['--hide-scrollbars', '--default-background-color=00000000', '--headless', '--disable-remote-debugging', '--no-sandbox'];
 
-hti.screenshot(other_file=input, save_as=output)
+# specify css with zero margin and padding and transparent html background
+css = '''
+html, body {
+    margin: 0;
+    padding: 0;
+    background-color: transparent;
+}
+'''
+
+print(f'{input} -> {output}')
+hti.screenshot(html_str=input, css_str=css, save_as=output)
