ffbada8060193d8003df9244334685bbe85cbe6f
Daniel Cazzulino <daniel@cazzulino.com>
2024-09-10 15:31:32 -0300

    Restore driver installation

--- .github/workflows/sponsors.py
+++ .github/workflows/sponsors.py
@@ -2,13 +2,12 @@ import os
 import sys
 from html2image import Html2Image
 
-chrome = sys.argv[1]
-
 # read entire contents of file in args[2] as input
 with open(sys.argv[2], 'r', encoding='utf-8') as file:
     input = file.read()
     
 output = sys.argv[3]
+chrome = sys.argv[1]
 
 hti = Html2Image(size=(39,39), browser_executable=chrome)
 hti.browser.print_command = True
